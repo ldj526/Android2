af0f0f2527ce793556c4695f6a6cb482ae6b2546
ldj5261@naver.com <2dlehdwns!>
2021-11-01 23:34:12 +0900

    .

--- app/src/main/java/com/example/myfirstmemoapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstmemoapp/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.example.myfirstmemoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.myfirstmemoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.fab.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(v: View?) {
-
-            }
-        })
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, WriteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/myfirstmemoapp/WriteActivity.kt
+++ app/src/main/java/com/example/myfirstmemoapp/WriteActivity.kt
@@ -2,10 +2,27 @@ package com.example.myfirstmemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.myfirstmemoapp.databinding.ActivityWriteBinding
 
 class WriteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write)
+        binding = ActivityWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            btnBack.setOnClickListener(object: View.OnClickListener {
+                override fun onClick(v: View?) {
+                    finish()
+                }
+            })
+            btnSave.setOnClickListener(object:View.OnClickListener {
+                override fun onClick(v: View?) {
+
+                }
+            })
+        }
     }
 }
