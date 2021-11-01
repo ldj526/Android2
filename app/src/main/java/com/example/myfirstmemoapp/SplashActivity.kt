75504947be7210494c3b950bd63bdfe94d349337
ldj5261@naver.com <2dlehdwns!>
2021-11-01 22:22:57 +0900

    .

---
+++ app/src/main/java/com/example/myfirstmemoapp/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myfirstmemoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)       // 2000(2s)은 몇 초 뒤에 화면이 넘어갈지 정해지는 것
+    }
+}
