22125c2817e7c5e83561e6edd9cd46165c3091eb
ldj5261@naver.com <2dlehdwns!>
2021-11-02 22:45:39 +0900

    .

--- app/src/main/java/com/example/myfirstmemoapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstmemoapp/MainActivity.kt
@@ -3,19 +3,40 @@ package com.example.myfirstmemoapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.LinearLayout
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myfirstmemoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val model: MemoViewModel by viewModels()
+    private lateinit var adapater: MemoAdapater
+    val datas = mutableListOf<Memo>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initRecyclerView()
+
         binding.fab.setOnClickListener {
             val intent = Intent(this, WriteActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun initRecyclerView() {
+        adapater = MemoAdapater(this)   // Adapter 생성
+        binding.recyclerView.adapter = adapater     // 화면의 RecyclerView와 연결
+        binding.recyclerView.layoutManager = GridLayoutManager(applicationContext, 3)
+
+        datas.add(Memo(intent.getStringExtra("title"),intent.getStringExtra("content")))
+
+        adapater.datas = datas
+        adapater.notifyDataSetChanged()     // 데이터를 신규 추가 or 삭제 시 adapter에게 값을 알려줌
     }
 }
--- app/src/main/java/com/example/myfirstmemoapp/WriteActivity.kt
+++ app/src/main/java/com/example/myfirstmemoapp/WriteActivity.kt
@@ -1,28 +1,34 @@
 package com.example.myfirstmemoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import com.example.myfirstmemoapp.databinding.ActivityWriteBinding
+import kotlinx.coroutines.Dispatchers
 
 class WriteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWriteBinding
+    private val model: MemoViewModel by viewModels()
+    lateinit var profileAdapter: MemoAdapater
+    private val datas = mutableListOf<Memo>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        with(binding){
-            btnBack.setOnClickListener(object: View.OnClickListener {
-                override fun onClick(v: View?) {
-                    finish()
-                }
-            })
-            btnSave.setOnClickListener(object:View.OnClickListener {
-                override fun onClick(v: View?) {
-
-                }
-            })
+        with(binding) {
+            btnBack.setOnClickListener {
+                finish()
+            }
+            btnSave.setOnClickListener {
+                var intent = Intent(this@WriteActivity, MainActivity::class.java)
+                intent.putExtra("title",etTitle.text.toString())
+                intent.putExtra("content",etContent.text.toString())
+                startActivity(intent)
+            }
         }
     }
 }
